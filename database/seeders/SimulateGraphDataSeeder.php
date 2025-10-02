621b94925effc38423e2c7958098246b3b855192
Chen, Danny <3949245+danny50610@users.noreply.github.com>
2025-10-02 11:32:22 +0800

    分離 song 和 youtube video，讓未來可支援一首歌有多 youtube 影片 (MV版、純歌曲版)

--- database/seeders/SimulateGraphDataSeeder.php
+++ database/seeders/SimulateGraphDataSeeder.php
@@ -16,11 +16,17 @@ class SimulateGraphDataSeeder extends Seeder
 {
     protected VertexType $vTuber;
     protected VertexProperty $vTuberPropertyName;
+
     protected VertexType $song;
     protected VertexProperty $songPropertyName;
-    protected VertexProperty $songPropertyYoutubeVideoId;
+
+    protected VertexType $youtubeVideo;
+    protected VertexProperty $youtubeVideoPropertyId;
+
     protected EdgeType $vocalEdge;
     protected EdgeProperty $vocalEdgePropertyOrder;
+
+    protected EdgeType $hasYoutubeVideoEdge;
 
     /**
      * Run the database seeds.
@@ -64,14 +70,20 @@ class SimulateGraphDataSeeder extends Seeder
         $this->songPropertyName->vertexType()->associate($this->song);
         $this->songPropertyName->save();
 
-        $this->songPropertyYoutubeVideoId = new VertexProperty([
-            'name' => 'Youtube Video MV Id',
+        $this->youtubeVideo = VertexType::create([
+            'name' => 'Youtube 影片',
             'description' => '',
-            'age_property_name' => 'youtube_video_id_mv',
+            'age_label_name' => 'youtube-video',
+        ]);
+
+        $this->youtubeVideoPropertyId = new VertexProperty([
+            'name' => 'Id',
+            'description' => '',
+            'age_property_name' => 'id',
             'age_property_type' => PropertyType::String,
         ]);
-        $this->songPropertyYoutubeVideoId->vertexType()->associate($this->song);
-        $this->songPropertyYoutubeVideoId->save();
+        $this->youtubeVideoPropertyId->vertexType()->associate($this->youtubeVideo);
+        $this->youtubeVideoPropertyId->save();
 
         $this->vocalEdge = new EdgeType([
             'name' => '主唱',
@@ -90,110 +102,104 @@ class SimulateGraphDataSeeder extends Seeder
         ]);
         $this->vocalEdgePropertyOrder->edgeType()->associate($this->vocalEdge);
         $this->vocalEdgePropertyOrder->save();
+
+        $this->hasYoutubeVideoEdge = new EdgeType([
+            'name' => 'Youtube 影片',
+            'description' => '',
+            'age_label_name' => 'has-youtube-video',
+        ]);
+        $this->hasYoutubeVideoEdge->startVertex()->associate($this->song);
+        $this->hasYoutubeVideoEdge->endVertex()->associate($this->youtubeVideo);
+        $this->hasYoutubeVideoEdge->save();
     }
 
     protected function createGraphData()
     {
-        // 星街すいせい - vocal -> 綺麗事 (https://www.youtube.com/watch?v=VPhLXeU25KA)
-        // AZKi - vocal -> いのち(2024 ver.) (https://www.youtube.com/watch?v=hQ3rYiaUsUY)
-        // AZKi × 星街すいせい - vocal -> The Last Frontier (https://www.youtube.com/watch?v=-9wUbw5qevU)
+        // 星街すいせい - vocal -> 綺麗事 - has-youtube-video -> (https://www.youtube.com/watch?v=VPhLXeU25KA)
+        // AZKi - vocal -> いのち(2024 ver.) - has-youtube-video -> (https://www.youtube.com/watch?v=hQ3rYiaUsUY)
+        // AZKi × 星街すいせい - vocal -> The Last Frontier - has-youtube-video -> (https://www.youtube.com/watch?v=-9wUbw5qevU)
 
-        DB::apacheAgeCypher(config('cohistograph.app.graph.name'), function (Builder $builder) {
+        $this->createVtuber('星街すいせい');
+        $this->createVtuber('AZKi');
+
+        $this->createSong('綺麗事');
+        $this->createSong('いのち(2024 ver.)');
+        $this->createSong('The Last Frontier');
+
+        $this->createYoutubeVideo('VPhLXeU25KA');
+        $this->createYoutubeVideo('hQ3rYiaUsUY');
+        $this->createYoutubeVideo('-9wUbw5qevU');
+
+        $this->createVocalEdge('星街すいせい', '綺麗事', 1);
+        $this->createVocalEdge('AZKi', 'いのち(2024 ver.)', 1);
+        $this->createVocalEdge('AZKi', 'The Last Frontier', 1);
+        $this->createVocalEdge('星街すいせい', 'The Last Frontier', 2);
+
+        $this->createHasYoutubeVideoEdge('綺麗事', 'VPhLXeU25KA');
+        $this->createHasYoutubeVideoEdge('いのち(2024 ver.)', 'hQ3rYiaUsUY');
+        $this->createHasYoutubeVideoEdge('The Last Frontier', '-9wUbw5qevU');
+    }
+
+    protected function createVtuber(string $name)
+    {
+         DB::apacheAgeCypher(config('cohistograph.app.graph.name'), function (Builder $builder) use ($name) {
             return $builder->createNode(null, $this->vTuber->age_label_name, [
-                $this->vTuberPropertyName->age_property_name => '星街すいせい',
+                $this->vTuberPropertyName->age_property_name => $name,
             ])->setAs(['v']);
         })->get();
+    }
 
-        DB::apacheAgeCypher(config('cohistograph.app.graph.name'), function (Builder $builder) {
-            return $builder->createNode(null, $this->vTuber->age_label_name, [
-                $this->vTuberPropertyName->age_property_name => 'AZKi',
-            ])->setAs(['v']);
-        })->get();
-
-        DB::apacheAgeCypher(config('cohistograph.app.graph.name'), function (Builder $builder) {
+    protected function createSong(string $name)
+    {
+         DB::apacheAgeCypher(config('cohistograph.app.graph.name'), function (Builder $builder) use ($name) {
             return $builder->createNode(null, $this->song->age_label_name, [
-                $this->songPropertyName->age_property_name => '綺麗事',
-                $this->songPropertyYoutubeVideoId->age_property_name => 'VPhLXeU25KA',
+                $this->songPropertyName->age_property_name => $name,
             ])->setAs(['v']);
         })->get();
+    }
 
-        DB::apacheAgeCypher(config('cohistograph.app.graph.name'), function (Builder $builder) {
-            return $builder->createNode(null, $this->song->age_label_name, [
-                $this->songPropertyName->age_property_name => 'いのち(2024 ver.)',
-                $this->songPropertyYoutubeVideoId->age_property_name => 'hQ3rYiaUsUY',
+    protected function createYoutubeVideo(string $id)
+    {
+         DB::apacheAgeCypher(config('cohistograph.app.graph.name'), function (Builder $builder) use ($id) {
+            return $builder->createNode(null, $this->youtubeVideo->age_label_name, [
+                $this->youtubeVideoPropertyId->age_property_name => $id,
             ])->setAs(['v']);
         })->get();
+    }
 
-        DB::apacheAgeCypher(config('cohistograph.app.graph.name'), function (Builder $builder) {
-            return $builder->createNode(null, $this->song->age_label_name, [
-                $this->songPropertyName->age_property_name => 'The Last Frontier',
-                $this->songPropertyYoutubeVideoId->age_property_name => '-9wUbw5qevU',
-            ])->setAs(['v']);
-        })->get();
-
-        DB::apacheAgeCypher(config('cohistograph.app.graph.name'), function (Builder $builder) {
+    protected function createVocalEdge(string $vtuberName, string $songName, int $order)
+    {
+        DB::apacheAgeCypher(config('cohistograph.app.graph.name'), function (Builder $builder) use ($vtuberName, $songName, $order) {
             return $builder
                 ->matchNode('a', $this->vTuber->age_label_name, [
-                    $this->vTuberPropertyName->age_property_name => '星街すいせい',
+                    $this->vTuberPropertyName->age_property_name => $vtuberName,
                 ])
                 ->matchNode('b', $this->song->age_label_name, [
-                    $this->songPropertyName->age_property_name => '綺麗事',
+                    $this->songPropertyName->age_property_name => $songName,
                 ])
                 ->createNode('a')
                 ->withCreateEdge(Direction::RIGHT, null, $this->vocalEdge->age_label_name, [
-                    $this->vocalEdgePropertyOrder->age_property_name => 1,
+                    $this->vocalEdgePropertyOrder->age_property_name => $order,
                 ])
                 ->withCreateNode('b')
                 ->setAs(['v']);
         })->get();
+    }
 
-        DB::apacheAgeCypher(config('cohistograph.app.graph.name'), function (Builder $builder) {
+    protected function createHasYoutubeVideoEdge(string $songName, string $youtubeVideoId)
+    {
+        DB::apacheAgeCypher(config('cohistograph.app.graph.name'), function (Builder $builder) use ($songName, $youtubeVideoId) {
             return $builder
-                ->matchNode('a', $this->vTuber->age_label_name, [
-                    $this->vTuberPropertyName->age_property_name => 'AZKi',
+                ->matchNode('a', $this->song->age_label_name, [
+                    $this->songPropertyName->age_property_name => $songName,
                 ])
-                ->matchNode('b', $this->song->age_label_name, [
-                    $this->songPropertyName->age_property_name => 'いのち(2024 ver.)',
+                ->matchNode('b', $this->youtubeVideo->age_label_name, [
+                    $this->youtubeVideoPropertyId->age_property_name => $youtubeVideoId,
                 ])
                 ->createNode('a')
-                ->withCreateEdge(Direction::RIGHT, null, $this->vocalEdge->age_label_name, [
-                    $this->vocalEdgePropertyOrder->age_property_name => 1,
-                ])
+                ->withCreateEdge(Direction::RIGHT, null, $this->hasYoutubeVideoEdge->age_label_name)
                 ->withCreateNode('b')
                 ->setAs(['v']);
         })->get();
-
-        DB::apacheAgeCypher(config('cohistograph.app.graph.name'), function (Builder $builder) {
-            return $builder
-                ->matchNode('a', $this->vTuber->age_label_name, [
-                    $this->vTuberPropertyName->age_property_name => 'AZKi',
-                ])
-                ->matchNode('b', $this->song->age_label_name, [
-                    $this->songPropertyName->age_property_name => 'The Last Frontier',
-                ])
-                ->createNode('a')
-                ->withCreateEdge(Direction::RIGHT, null, $this->vocalEdge->age_label_name, [
-                    $this->vocalEdgePropertyOrder->age_property_name => 1,
-                ])
-                ->withCreateNode('b')
-                ->setAs(['v']);
-        })->get();
-
-        DB::apacheAgeCypher(config('cohistograph.app.graph.name'), function (Builder $builder) {
-            return $builder
-                ->matchNode('a', $this->vTuber->age_label_name, [
-                    $this->vTuberPropertyName->age_property_name => '星街すいせい',
-                ])
-                ->matchNode('b', $this->song->age_label_name, [
-                    $this->songPropertyName->age_property_name => 'The Last Frontier',
-                ])
-                ->createNode('a')
-                ->withCreateEdge(Direction::RIGHT, null, $this->vocalEdge->age_label_name, [
-                    $this->vocalEdgePropertyOrder->age_property_name => 2,
-                ])
-                ->withCreateNode('b')
-                ->setAs(['v']);
-        })->get();
-
     }
 }
