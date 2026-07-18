0c94526e04a919d7c4c845d7f487a6f306812656
Chen, Danny <3949245+danny50610@users.noreply.github.com>
2026-07-18 11:11:07 +0000

    rename to REDIS_DATABASE_INDEX_BASE

--- config/database.php
+++ config/database.php
@@ -172,7 +172,7 @@ return [
             'username' => env('REDIS_USERNAME'),
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_DB', '0'),
+            'database' => env('REDIS_DATABASE_INDEX_BASE', 0),
         ],
 
         'cache' => [
@@ -181,7 +181,7 @@ return [
             'username' => env('REDIS_USERNAME'),
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_CACHE_DB', '1'),
+            'database' => env('REDIS_DATABASE_INDEX_BASE', 0) + 1,
         ],
 
     ],
