19189d9a34349e2df278c59afabaa6d4af85d624
Chen, Danny <3949245+danny50610@users.noreply.github.com>
2026-07-19 05:11:11 +0000

    try output call

--- routes/console.php
+++ routes/console.php
@@ -3,11 +3,11 @@
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('before-phpunit-setup', function () {
-    Artisan::call('migrate', [
+    $this->call('migrate', [
         '--database' => 'pgsql-age',
         '--path' => 'database/migrations-age',
         '--force' => true,
     ]);
 
-    Artisan::call('app:apply-role-and-permission-command');
+    $this->call('app:apply-role-and-permission-command');
 })->purpose('Prepare AGE graph and roles/permissions before PHPUnit');
